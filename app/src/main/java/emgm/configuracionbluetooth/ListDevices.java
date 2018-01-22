d410383063a78843d081404725a42c78d8a773e8
DESARROLLO-07 <emgonzalezm@ufpso.edu.co>
2018-01-22 16:03:29 -0500

    Commit

--- app/src/main/java/emgm/configuracionbluetooth/ListDevices.java
+++ app/src/main/java/emgm/configuracionbluetooth/ListDevices.java
@@ -1,8 +1,18 @@
 package emgm.configuracionbluetooth;
 
 import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
 
 /**
  * Created by DESARROLLO-07 on 22/01/2018.
@@ -10,8 +20,51 @@ import android.support.annotation.Nullable;
 
 public class ListDevices extends ListActivity {
 
+    BluetoothAdapter mBluetoothAdapter2 = null;
+
+    static String DIRECCION_MAC = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ArrayAdapter<String> ArrayBluetooth = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+        mBluetoothAdapter2 = BluetoothAdapter.getDefaultAdapter();
+
+        Set<BluetoothDevice> dispositivosDisponibles = mBluetoothAdapter2.getBondedDevices();
+
+        if(dispositivosDisponibles.size() > 0){
+
+            for(BluetoothDevice dispositivo: dispositivosDisponibles){
+
+                String nameBt = dispositivo.getName();
+                String macBt = dispositivo.getAddress();
+
+                ArrayBluetooth.add(nameBt + "\n" + macBt);
+
+            }
+        }
+
+        setListAdapter(ArrayBluetooth);
+    }
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String información  = ((TextView) v).getText().toString();
+
+        String direccionMac = información.substring(información.length() - 17);
+
+        //Toast.makeText(this, "Mac: " + direccionMac, Toast.LENGTH_SHORT).show();
+
+        Intent returMac = new Intent();
+        returMac.putExtra(DIRECCION_MAC, direccionMac);
+        setResult(RESULT_OK, returMac);
+        finish();
+
+
     }
 }
--- app/src/main/java/emgm/configuracionbluetooth/MainActivity.java
+++ app/src/main/java/emgm/configuracionbluetooth/MainActivity.java
@@ -2,6 +2,8 @@ package emgm.configuracionbluetooth;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,15 +11,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnConnect, btnDisconecct, btnEncenderLeds, btnApagarLeds;
 
-    private BluetoothAdapter mBluetoothAdapter = null; /// Adaptador
+    BluetoothAdapter mBluetoothAdapter = null; /// Adaptador
+    BluetoothDevice  mBTDevice = null;
+    BluetoothSocket  mBTSocket = null;
 
     private static final int SOLICITA_ACTIVACION = 1;
     private static final int SOLICITA_CONEXION = 2;
 
     boolean conexionBluetooth = false;
+
+    private static String MAC = null;
+
+    UUID MEU_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +88,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 break;
 
+            case SOLICITA_CONEXION:
+
+                if(resultCode == Activity.RESULT_OK){
+
+                    MAC = data.getExtras().getString(ListDevices.DIRECCION_MAC);
+
+                    Toast.makeText(getApplicationContext(), "MAC Obtenida: " + MAC, Toast.LENGTH_SHORT).show();
+                    mBTDevice = mBluetoothAdapter.getRemoteDevice(MAC);
+
+                    try {
+
+                        mBTSocket = mBTDevice.createRfcommSocketToServiceRecord(MEU_UUID);
+
+                        mBTSocket.connect();
+
+                        conexionBluetooth = true;
+
+                        btnConnect.setText("Desconectar");
+
+                        Toast.makeText(getApplicationContext(), "Dispositivo conectado con: " + MAC, Toast.LENGTH_SHORT).show();
+
+                    }catch (IOException erro){
+
+                        conexionBluetooth = false;
+
+                        Toast.makeText(getApplicationContext(), "Ha ocurrido un error: " + erro, Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "Falla al obtener la MAC", Toast.LENGTH_SHORT).show();
+
+                }
+
+                break;
+
         }
 
     }
@@ -88,8 +135,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnConnect:
 
                 if(conexionBluetooth){
-
                     // Desconectar
+
+                    try {
+
+                        mBTSocket.close();
+                        Toast.makeText(getApplicationContext(), "Bluetooth fue desconectado", Toast.LENGTH_SHORT).show();
+
+                        conexionBluetooth = false;
+
+                        btnConnect.setText("Conectar");
+
+                    }catch (IOException erro){
+                        Toast.makeText(getApplicationContext(), "Ha ocurrido un error: " + erro, Toast.LENGTH_SHORT).show();
+                    }
 
                 }else{
                     // Conectar
