120b9aea21a840e8fa21bc19afceda4639539a85
DESARROLLO-07 <emgonzalezm@ufpso.edu.co>
2018-01-22 16:57:04 -0500

    Commit

--- app/src/main/java/emgm/configuracionbluetooth/MainActivity.java
+++ app/src/main/java/emgm/configuracionbluetooth/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private static final int SOLICITA_ACTIVACION = 1;
     private static final int SOLICITA_CONEXION = 2;
+
+    ConnectedThread connectedThread;
 
     boolean conexionBluetooth = false;
 
@@ -105,6 +109,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                         conexionBluetooth = true;
 
+                        connectedThread = new ConnectedThread(mBTSocket);
+                        connectedThread.start();
+
                         btnConnect.setText("Desconectar");
 
                         Toast.makeText(getApplicationContext(), "Dispositivo conectado con: " + MAC, Toast.LENGTH_SHORT).show();
@@ -165,11 +172,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.btnEncenderLeds:
 
+                if(conexionBluetooth){
+
+                    connectedThread.enviarDatosArduino("ledsOn");
+
+                }else{
+
+                    Toast.makeText(getApplicationContext(), "Bluetooth no esta conectado", Toast.LENGTH_SHORT).show();
+
+                }
+
                 break;
 
             case R.id.btnApagarLeds:
 
                 break;
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+                    // Send the obtained bytes to the UI activity
+                    //mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                          //  .sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void enviarDatosArduino(String datosParaEnviar) {
+
+            byte[] msgBuffer = datosParaEnviar.getBytes();
+
+            try {
+                mmOutStream.write(msgBuffer);
+            } catch (IOException e) {
+
+            }
         }
     }
 }
